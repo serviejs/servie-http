091f4b3b23a72d300ab49da85912c838fb4ad4d6
Blake Embrey <hello@blakeembrey.com>
2017-03-17 13:13:31 -0700

    Add support for sending trailers

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,19 +1,58 @@
 import { Response } from 'servie'
 import { get } from 'servie-route'
-import { request } from 'popsicle'
-import popsicleServer = require('popsicle-server')
+import { compose } from 'throwback'
+import { createReadStream } from 'fs'
+import * as http from 'http'
+import { join } from 'path'
 import { createHandler } from './index'
 
 describe('servie-http', () => {
-  const handler = createHandler(get('/', () => {
-    return new Response({ body: 'hello world' })
-  }))
-
-  it('should work over http', () => {
-    return request('/')
-      .use(popsicleServer(handler))
-      .then((res) => {
-        expect(res.body).toBe('hello world')
+  const handler = createHandler(compose([
+    get('/', () => {
+      return new Response({ body: 'hello world' })
+    }),
+    get('/stream', () => {
+      return new Response({
+        body: createReadStream(join(__dirname, 'index.ts')),
+        headers: {
+          'Trailer': 'Server-Timing'
+        },
+        trailers: {
+          'Server-Timing': 'end=100'
+        }
       })
+    })
+  ]))
+
+  const server = http.createServer(handler).listen(0)
+
+  afterAll(() => server.close())
+
+  it('should work over http', (done) => {
+    return http.get(`http://localhost:${server.address().port}`, (res) => {
+      let data = ''
+
+      res.on('data', (chunk: Buffer) => data += chunk.toString('utf8'))
+
+      res.on('end', () => {
+        expect(res.headers['content-type']).toEqual('text/plain')
+        expect(res.headers['content-length']).toEqual('11')
+        expect(data).toBe('hello world')
+
+        return done()
+      })
+    })
+  })
+
+  it('should send trailers', (done) => {
+    return http.get(`http://localhost:${server.address().port}/stream`, (res) => {
+      res.resume()
+
+      res.on('end', () => {
+        expect(res.trailers).toEqual({ 'server-timing': 'end=100' })
+
+        return done()
+      })
+    })
   })
 })
